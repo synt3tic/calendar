972c93c0af3a9024ef07e3e7098fdd130c423b14
Vlad Sapozhnikov <mel04nax@gmail.com>
2024-02-16 22:05:22 +0300

    adding dist 3

--- dist/assets/keyboard2-legacy-54d0b76b.js
+++ dist/assets/keyboard2-legacy-54d0b76b.js
@@ -1,4 +1,4 @@
-System.register(["./keyboard-legacy-e9515c75.js","./index-legacy-06e3c6ad.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K},null],execute:function(){
+System.register(["./keyboard-legacy-33f41d39.js","./index-legacy-5a348bd1.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K},null],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
